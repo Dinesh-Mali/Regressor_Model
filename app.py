f88ce68e19e0a4b942f7960db7e171426238c1dc
diinesh17malii@gmail.com <diinesh17malii@gmail.com>
2021-12-27 09:49:55 +0530

    function modification

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def predict_height():
     input_data = np.array([[2],[3],[22],[10]])
     model = load('model.joblib')
     predicts = model.predict(input_data)
